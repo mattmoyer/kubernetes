4f709d5035fbd0d2489c1ca1a45a12a30fcce2dd
Matt Moyer <moyer@heptio.com>
2017-07-24 16:54:28 -0500

    kubeadm: implement TLS discovery root CA pinning.
    This introduces enhanced TLS validation using the `--tls-discovery-root-ca` public key hashes. This is done by connecting to the cluster-info endpoint using an unvalidated/unsafe TLS connection, but saving the certificate chain for post-hoc validation. After the cluster info has been loaded and parsed, the root CA is validated against the pinned public key set, then that CA is used to retroactively validate the original TLS certificate chain.

--- cmd/kubeadm/app/discovery/token/token.go
+++ cmd/kubeadm/app/discovery/token/token.go
@@ -17,7 +17,14 @@ limitations under the License.
 package token
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"encoding/pem"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"sync"
 
 	"k8s.io/api/core/v1"
@@ -27,6 +34,7 @@ import (
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 	"k8s.io/kubernetes/cmd/kubeadm/app/constants"
 	kubeconfigutil "k8s.io/kubernetes/cmd/kubeadm/app/util/kubeconfig"
+	"k8s.io/kubernetes/cmd/kubeadm/app/util/pubkeypin"
 	tokenutil "k8s.io/kubernetes/cmd/kubeadm/app/util/token"
 	bootstrapapi "k8s.io/kubernetes/pkg/bootstrap/api"
 	"k8s.io/kubernetes/pkg/controller/bootstrap"
@@ -34,11 +42,25 @@ import (
 
 const BootstrapUser = "token-bootstrap-client"
 
+var (
+	errInsecureClientUsedMoreThanOnce = fmt.Errorf("client must be used for only a single request")
+	errInsecureClientEmptyCertChain   = fmt.Errorf("expected at least one server certificate")
+	errInsecureClientNoRequest        = fmt.Errorf("doRequest did not make a request using the withInsecureHTTPClient client")
+	errInvalidPEMData                 = fmt.Errorf("invalid PEM data")
+	errTrailingPEMData                = fmt.Errorf("trailing data after first PEM block")
+)
+
 // RetrieveValidatedClusterInfo connects to the API Server and tries to fetch the cluster-info ConfigMap
 // It then makes sure it can trust the API Server by looking at the JWS-signed tokens
-func RetrieveValidatedClusterInfo(discoveryToken string, tokenAPIServers []string) (*clientcmdapi.Cluster, error) {
-
+func RetrieveValidatedClusterInfo(discoveryToken string, tokenAPIServers, rootCAPubKeys []string) (*clientcmdapi.Cluster, error) {
 	tokenId, tokenSecret, err := tokenutil.ParseToken(discoveryToken)
+	if err != nil {
+		return nil, err
+	}
+
+	// Load the cfg.TLSDiscoveryRootCAPubKeys into a pubkeypin.Set
+	pubKeyPins := pubkeypin.NewSet()
+	err = pubKeyPins.Allow(rootCAPubKeys...)
 	if err != nil {
 		return nil, err
 	}
@@ -47,24 +69,46 @@ func RetrieveValidatedClusterInfo(discoveryToken string, tokenAPIServers []strin
 	// The endpoint that wins the race and completes the task first gets its kubeconfig returned below
 	baseKubeConfig := runForEndpointsAndReturnFirst(tokenAPIServers, func(endpoint string) (*clientcmdapi.Config, error) {
 
-		bootstrapConfig := buildInsecureBootstrapKubeConfig(endpoint)
-		clusterName := bootstrapConfig.Contexts[bootstrapConfig.CurrentContext].Cluster
-
-		client, err := kubeconfigutil.KubeConfigToClientSet(bootstrapConfig)
+		// clusterInfoURL is the URL of the bootstrap cluster info ConfigMap on the Kubernetes API
+		clusterInfoURL, err := url.Parse(fmt.Sprintf(
+			"https://%s/api/v1/namespaces/%s/configmaps/%s",
+			endpoint,
+			metav1.NamespacePublic,
+			bootstrapapi.ConfigMapClusterInfo))
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("invalid URL, can't connect: %v", err)
 		}
-
-		fmt.Printf("[discovery] Created cluster-info discovery client, requesting info from %q\n", bootstrapConfig.Clusters[clusterName].Server)
+		fmt.Printf("[discovery] Requesting cluster-info from %q\n", clusterInfoURL)
 
 		var clusterinfo *v1.ConfigMap
+		var bootstrapCertificateChain []*x509.Certificate
+
 		wait.PollImmediateInfinite(constants.DiscoveryRetryInterval, func() (bool, error) {
-			var err error
-			clusterinfo, err = client.CoreV1().ConfigMaps(metav1.NamespacePublic).Get(bootstrapapi.ConfigMapClusterInfo, metav1.GetOptions{})
+			response, certificates, err := withInsecureHTTPClient(func(client *http.Client) (*http.Response, error) {
+				return client.Get(clusterInfoURL.String())
+			})
 			if err != nil {
 				fmt.Printf("[discovery] Failed to request cluster info, will try again: [%s]\n", err)
 				return false, nil
 			}
+			defer response.Body.Close()
+
+			// Read the entire response body
+			responseBytes, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				fmt.Printf("[discovery] Failed to read cluster info response, will try again: [%s]\n", err)
+				return false, nil
+			}
+
+			// Parse the JSON into a ConfigMap
+			err = json.Unmarshal(responseBytes, &clusterinfo)
+			if err != nil {
+				fmt.Printf("[discovery] Failed to parse cluster info response, will try again: [%s]\n", err)
+				return false, nil
+			}
+
+			// Success, so save off the certificate chain for later validation
+			bootstrapCertificateChain = certificates
 			return true, nil
 		})
 
@@ -85,20 +129,50 @@ func RetrieveValidatedClusterInfo(discoveryToken string, tokenAPIServers []strin
 			return nil, fmt.Errorf("couldn't parse the kubeconfig file in the %s configmap: %v", bootstrapapi.ConfigMapClusterInfo, err)
 		}
 
-		fmt.Printf("[discovery] Cluster info signature and contents are valid, will use API Server %q\n", bootstrapConfig.Clusters[clusterName].Server)
+		// If no TLS root CA pinning was specified, we're done.
+		if pubKeyPins.Empty() {
+			fmt.Printf("[discovery] Cluster info signature and contents are valid and no TLS pinning was specified, will use API Server %q\n", endpoint)
+			return finalConfig, nil
+		}
+
+		// For each cluster, validate that its CA matches a pinned key and save that root into an x509.CertPool
+		pinnedRoots := x509.NewCertPool()
+		for _, cluster := range finalConfig.Clusters {
+			caCert, err := getClusterCA(cluster)
+			if err != nil {
+				return nil, fmt.Errorf("could not get cluster CA from %s configmap: %v", bootstrapapi.ConfigMapClusterInfo, err)
+			}
+
+			err = pubKeyPins.Check(caCert)
+			if err != nil {
+				return nil, fmt.Errorf("unknown cluster CA: %v", err)
+			}
+
+			pinnedRoots.AddCert(caCert)
+		}
+
+		// Build an intermediate CA pool containing all the non-leaf certificates sent by the server
+		chainCerts := x509.NewCertPool()
+		for _, chainCert := range bootstrapCertificateChain[1:] {
+			chainCerts.AddCert(chainCert)
+		}
+
+		// Now that we know the root CA pool, validate the original certificate chain
+		// against the server's hostname
+		_, err = bootstrapCertificateChain[0].Verify(x509.VerifyOptions{
+			DNSName:       clusterInfoURL.Hostname(),
+			Roots:         pinnedRoots,
+			Intermediates: chainCerts,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("server certificate is not valid for expected hostname %q", clusterInfoURL.Hostname())
+		}
+
+		fmt.Printf("[discovery] Cluster info signature and contents are valid and TLS certificate validates against pinned roots, will use API Server %q\n", endpoint)
 		return finalConfig, nil
 	})
 
 	return kubeconfigutil.GetClusterFromKubeConfig(baseKubeConfig), nil
-}
-
-// buildInsecureBootstrapKubeConfig makes a KubeConfig object that connects insecurely to the API Server for bootstrapping purposes
-func buildInsecureBootstrapKubeConfig(endpoint string) *clientcmdapi.Config {
-	masterEndpoint := fmt.Sprintf("https://%s", endpoint)
-	clusterName := "kubernetes"
-	bootstrapConfig := kubeconfigutil.CreateBasic(masterEndpoint, clusterName, BootstrapUser, []byte{})
-	bootstrapConfig.Clusters[clusterName].InsecureSkipTLSVerify = true
-	return bootstrapConfig
 }
 
 // runForEndpointsAndReturnFirst loops the endpoints slice and let's the endpoints race for connecting to the master
@@ -130,4 +204,77 @@ func runForEndpointsAndReturnFirst(endpoints []string, fetchKubeConfigFunc func(
 	}
 	wg.Wait()
 	return resultingKubeConfig
+}
+
+// certChain is an array of *x509.Certificate with a helper to plug into the tls VerifyPeerCertificate hook
+type certChain []*x509.Certificate
+
+// saveCertificateChain parses and saves the array of DER-encoded certificates into a certChain
+func (c *certChain) saveCertificateChain(rawCerts [][]byte, _ [][]*x509.Certificate) error {
+	// Make sure this wrapper is being used correctly
+	if len(*c) != 0 {
+		return errInsecureClientUsedMoreThanOnce
+	}
+
+	// Do a quick sanity check (this should never happen)
+	if len(rawCerts) < 1 {
+		return errInsecureClientEmptyCertChain
+	}
+
+	// Parse and collect each certificate
+	for _, rawCert := range rawCerts {
+		cert, err := x509.ParseCertificate(rawCert)
+		if err != nil {
+			return err
+		}
+		*c = append(*c, cert)
+	}
+
+	return nil
+}
+
+// withInsecureHTTPClient creates a temporary http.Client, passes it to doRequest(...),
+// which should perform a single HTTP request and return the response. The temporary
+// client will not validate TLS certificates in real time, but instead will capture
+// and return the certificates for post-hoc validation.
+func withInsecureHTTPClient(
+	doRequest func(*http.Client) (*http.Response, error),
+) (*http.Response, []*x509.Certificate, error) {
+
+	// certificates will collect the certificate chain presented by the server
+	var certificates certChain
+
+	// Create an HTTP client with a custom TLS transport that saves off the certificate chain for later validation
+	insecureClient := http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify:    true,
+				VerifyPeerCertificate: certificates.saveCertificateChain,
+			},
+		},
+	}
+
+	// Perform what should be a single request using the client
+	response, err := doRequest(&insecureClient)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Make sure the wrapper is being used correctly
+	if len(certificates) == 0 {
+		return nil, nil, errInsecureClientNoRequest
+	}
+	return response, certificates, nil
+}
+
+// getClusterCA extracts and decodes the root CA section from a clientcmdapi.Cluster
+func getClusterCA(cluster *clientcmdapi.Cluster) (*x509.Certificate, error) {
+	pemBlock, trailingData := pem.Decode(cluster.CertificateAuthorityData)
+	if pemBlock == nil {
+		return nil, errInvalidPEMData
+	}
+	if len(trailingData) != 0 {
+		return nil, errTrailingPEMData
+	}
+	return x509.ParseCertificate(pemBlock.Bytes)
 }
--- cmd/kubeadm/app/discovery/token/token_test.go
+++ cmd/kubeadm/app/discovery/token/token_test.go
@@ -17,13 +17,45 @@ limitations under the License.
 package token
 
 import (
+	"encoding/pem"
+	"fmt"
+	"net/http"
 	"strconv"
+	"strings"
 	"testing"
 	"time"
 
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 	kubeconfigutil "k8s.io/kubernetes/cmd/kubeadm/app/util/kubeconfig"
 )
+
+// testCertPEM is a simple self-signed test certificate issued with the openssl CLI:
+// openssl req -new -newkey rsa:2048 -days 36500 -nodes -x509 -keyout /dev/null -out test.crt
+const testCertPEM = `
+-----BEGIN CERTIFICATE-----
+MIIDRDCCAiygAwIBAgIJAJgVaCXvC6HkMA0GCSqGSIb3DQEBBQUAMB8xHTAbBgNV
+BAMTFGt1YmVhZG0ta2V5cGlucy10ZXN0MCAXDTE3MDcwNTE3NDMxMFoYDzIxMTcw
+NjExMTc0MzEwWjAfMR0wGwYDVQQDExRrdWJlYWRtLWtleXBpbnMtdGVzdDCCASIw
+DQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAK0ba8mHU9UtYlzM1Own2Fk/XGjR
+J4uJQvSeGLtz1hID1IA0dLwruvgLCPadXEOw/f/IWIWcmT+ZmvIHZKa/woq2iHi5
++HLhXs7aG4tjKGLYhag1hLjBI7icqV7ovkjdGAt9pWkxEzhIYClFMXDjKpMSynu+
+YX6nZ9tic1cOkHmx2yiZdMkuriRQnpTOa7bb03OC1VfGl7gHlOAIYaj4539WCOr8
++ACTUMJUFEHcRZ2o8a/v6F9GMK+7SC8SJUI+GuroXqlMAdhEv4lX5Co52enYaClN
++D9FJLRpBv2YfiCQdJRaiTvCBSxEFz6BN+PtP5l2Hs703ZWEkOqCByM6HV8CAwEA
+AaOBgDB+MB0GA1UdDgQWBBRQgUX8MhK2rWBWQiPHWcKzoWDH5DBPBgNVHSMESDBG
+gBRQgUX8MhK2rWBWQiPHWcKzoWDH5KEjpCEwHzEdMBsGA1UEAxMUa3ViZWFkbS1r
+ZXlwaW5zLXRlc3SCCQCYFWgl7wuh5DAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEB
+BQUAA4IBAQCaAUif7Pfx3X0F08cxhx8/Hdx4jcJw6MCq6iq6rsXM32ge43t8OHKC
+pJW08dk58a3O1YQSMMvD6GJDAiAfXzfwcwY6j258b1ZlI9Ag0VokvhMl/XfdCsdh
+AWImnL1t4hvU5jLaImUUMlYxMcSfHBGAm7WJIZ2LdEfg6YWfZh+WGbg1W7uxLxk6
+y4h5rWdNnzBHWAGf7zJ0oEDV6W6RSwNXtC0JNnLaeIUm/6xdSddJlQPwUv8YH4jX
+c1vuFqTnJBPcb7W//R/GI2Paicm1cmns9NLnPR35exHxFTy+D1yxmGokpoPMdife
+aH+sfuxT8xeTPb3kjzF9eJTlnEquUDLM
+-----END CERTIFICATE-----`
+
+var testCertPEMBlock, _ = pem.Decode([]byte(testCertPEM))
+
+var testCertDER = testCertPEMBlock.Bytes
 
 func TestRunForEndpointsAndReturnFirst(t *testing.T) {
 	tests := []struct {
@@ -57,5 +89,128 @@ func TestRunForEndpointsAndReturnFirst(t *testing.T) {
 				rt.expectedEndpoint,
 			)
 		}
+	}
+}
+
+func TestGetClusterCA(t *testing.T) {
+	for _, testCase := range []struct {
+		name        string
+		input       []byte
+		expectValid bool
+	}{
+		{"invalid certificate data", []byte{0}, false},
+		{"certificate with junk appended", []byte(testCertPEM + "\nABC"), false},
+		{"multiple certificates", []byte(testCertPEM + "\n" + testCertPEM), false},
+		{"valid", []byte(testCertPEM), true},
+	} {
+		cert, err := getClusterCA(&clientcmdapi.Cluster{
+			CertificateAuthorityData: testCase.input,
+		})
+		if testCase.expectValid {
+			if err != nil {
+				t.Errorf("failed TestGetClusterCA(%s): unexpected error %v", testCase.name, err)
+			}
+			if cert == nil {
+				t.Errorf("failed TestGetClusterCA(%s): returned nil", testCase.name)
+			}
+		} else {
+			if err == nil {
+				t.Errorf("failed TestGetClusterCA(%s): expected an error", testCase.name)
+			}
+			if cert != nil {
+				t.Errorf("failed TestGetClusterCA(%s): expected not to get a certificate back, but got one", testCase.name)
+			}
+		}
+	}
+}
+
+func TestSaveCertificateChainSuccess(t *testing.T) {
+	var chain certChain
+	var testCerts = [][]byte{testCertDER}
+	err := chain.saveCertificateChain(testCerts, nil)
+	if err != nil {
+		t.Errorf("saveCertificateChain: unexpected error: %v", err)
+	}
+	if len(chain) != 1 {
+		t.Errorf("saveCertificateChain: expected a single certificate in the chain, got %v", chain)
+	}
+}
+
+func TestSaveCertificateChainReuse(t *testing.T) {
+	var chain certChain
+	var testCerts = [][]byte{testCertDER}
+	chain.saveCertificateChain(testCerts, nil)
+	t.Logf("chain: %v", chain)
+	err := chain.saveCertificateChain(testCerts, nil)
+	t.Logf("chain: %v", chain)
+	if err != errInsecureClientUsedMoreThanOnce {
+		t.Errorf("saveCertificateChain: expected errInsecureClientUsedMoreThanOnce, but got %v", err)
+	}
+
+}
+
+func TestSaveCertificateChainInvalid(t *testing.T) {
+	var chain certChain
+	var zero = [][]byte{{0}}
+	err := chain.saveCertificateChain(zero, nil)
+	if err == nil || !strings.Contains(err.Error(), "asn1") {
+		t.Errorf("saveCertificateChain: expected an asn.1 error, but got %v", err)
+	}
+}
+
+func TestSaveCertificateChainEmpty(t *testing.T) {
+	var chain certChain
+	var empty [][]byte
+	err := chain.saveCertificateChain(empty, nil)
+	if err != errInsecureClientEmptyCertChain {
+		t.Errorf("saveCertificateChain: expected errInsecureClientEmptyCertChain, but got %v", err)
+	}
+}
+
+func TestWithInsecureHTTPClientSuccess(t *testing.T) {
+	responseIn := http.Response{}
+	responseOut, certs, err := withInsecureHTTPClient(func(c *http.Client) (*http.Response, error) {
+		c.Transport.(*http.Transport).TLSClientConfig.VerifyPeerCertificate([][]byte{testCertDER}, nil)
+		return &responseIn, nil
+	})
+	if responseOut != &responseIn {
+		t.Errorf("expected response to be passed through")
+	}
+	if len(certs) != 1 {
+		t.Errorf("expected certs to have a single item")
+	}
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+}
+
+func TestWithInsecureHTTPClientInnerError(t *testing.T) {
+	testErr := fmt.Errorf("test")
+	response, certs, err := withInsecureHTTPClient(func(*http.Client) (*http.Response, error) {
+		return nil, testErr
+	})
+	if response != nil {
+		t.Errorf("expected nil response")
+	}
+	if certs != nil {
+		t.Errorf("expected nil certs")
+	}
+	if err != testErr {
+		t.Errorf("expected testErr, got %v", err)
+	}
+}
+
+func TestWithInsecureHTTPClientNoCall(t *testing.T) {
+	response, certs, err := withInsecureHTTPClient(func(*http.Client) (*http.Response, error) {
+		return nil, nil
+	})
+	if response != nil {
+		t.Errorf("expected nil response")
+	}
+	if certs != nil {
+		t.Errorf("expected nil certs")
+	}
+	if err != errInsecureClientNoRequest {
+		t.Errorf("expected errInsecureClientNoRequest, got %v", err)
 	}
 }
